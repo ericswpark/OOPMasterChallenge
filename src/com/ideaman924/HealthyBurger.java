e1f5dad95d2658d18130bc6cc873f8a77cbddc37
Eric Park <ideamaneric@gmail.com>
2020-03-03 03:55:21 +0900

    Realize you haven't finished HealthyBurger yet

--- src/com/ideaman924/HealthyBurger.java
+++ src/com/ideaman924/HealthyBurger.java
@@ -2,9 +2,25 @@ package com.ideaman924;
 
 public class HealthyBurger extends Hamburger {
     private int shrimp;
-    private int
+    private int avocado;
 
     public HealthyBurger(int meat, int price) {
         super("Brown Rye Bread Roll", meat, price);
+    }
+
+    public int getShrimp() {
+        return shrimp;
+    }
+
+    public void setShrimp(int shrimp) {
+        this.shrimp = shrimp;
+    }
+
+    public int getAvocado() {
+        return avocado;
+    }
+
+    public void setAvocado(int avocado) {
+        this.avocado = avocado;
     }
 }
